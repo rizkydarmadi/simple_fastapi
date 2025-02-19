3059a04e4e2df0053fd342b41a747e26445899be
rizkydarmadi <rizkydarmadi69@gmail.com>
2025-02-19 17:42:40 +0700

    fix: remove import setting

--- database.py
+++ database.py
@@ -1,7 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from settings import Settings
 
 engine = create_engine(
     "postgresql+psycopg://myuser:mypassword@db:5432/mydatabase",
